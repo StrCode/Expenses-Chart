0199ec1b4b1b18cdf01d5ac239a00e7cb4339a4b
Bex IT Digital Solutions <info@bexit.com.ng>
2024-01-25 10:01:32 +0100

    fix accessibility issues and width issues

--- src/App.tsx
+++ src/App.tsx
@@ -11,31 +11,31 @@ function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen space-y-5 bg-mediumBrown">
-        <div className="flex justify-between px-6 py-4 bg-soft w-96 text-cream rounded-xl">
+        <div className="flex justify-between px-6 py-4 bg-soft w-[500px] text-cream rounded-xl">
           <div className="flex flex-col">
             <p className="text-xs ">My balance</p>
             <p className="text-2xl">$921.48</p>
           </div>
           <img src={logo} width="50px" height="50px" alt="" />
         </div>
-        <div className="px-6 py-6 space-y-8 bg-white rounded-lg w-96">
+        <div className="px-6 py-6 space-y-8 bg-white rounded-lg w-[500px] ">
           <div className="text-2xl text-darkBrown">Spending - Last 7 days</div>
           <ul className="grid grid-cols-7 gap-3 text-[10px] min-h-36 text-mediumBrown ">
             {data.map((item: Expense) => (
-              <div
+              <li
                 key={item.day}
                 className="flex flex-col-reverse gap-2 text-center "
               >
                 <span>{item.day}</span>
-                <motion.li
+                <motion.div
                   layout
                   animate={{ height: [0, item.height] }}
                   transition={{
                     duration: 0.8,
                   }}
                   className="flex flex-col-reverse items-center rounded-md bg-soft"
-                ></motion.li>
-              </div>
+                ></motion.div>
+              </li>
             ))}
           </ul>
           <div className="border border-mediumBorder"></div>
